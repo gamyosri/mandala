d6965075d9df870f05fe156588d08c0cf29bb597
yosrigam <s33636@beuth-hochschule.de>
2021-03-25 23:55:34 +0100

    update appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            $url->forceSchema('https');
+        }
     }
 }
