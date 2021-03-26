dad608e8a307b19ac918a4d9be2495a05dab5270
yosrigam <s33636@beuth-hochschule.de>
2021-03-27 00:18:43 +0100

    add categories nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,35 +14,61 @@
 </head>
 
 <body>
-<header class="lg:px-38 px-16 bg-pink-200 flex flex-wrap items-center lg:py-10 py-5">
-    <div class="flex-1 flex justify-between items-center">
-        <a href="#">
-            Mandala
-        </a>
-    </div>
+<div class="bg-pink-200 ">
+    <header class="lg:px-38 px-16 text-indigo-50 flex flex-wrap items-center lg:py-10 py-5">
+        <div class="flex-1 flex justify-between items-center">
+            <a href="#">
+                Mandala
+            </a>
+        </div>
 
-    <label for="menu-toggle" class="pointer-cursor lg:hidden block">
-        <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-             viewBox="0 0 20 20"><title>menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg>
-    </label>
-    <input class="hidden" type="checkbox" id="menu-toggle"/>
+        <label for="menu-toggle" class="pointer-cursor lg:hidden block">
+            <svg class="fill-current text-indigo-50" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                 viewBox="0 0 20 20"><title>menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+        </label>
+        <input class="hidden" type="checkbox" id="menu-toggle"/>
 
-    <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
-        <nav>
-            <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Home</a>
-                </li>
-                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Categories</a>
-                </li>
-                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">About</a>
-                </li>
-                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
-                       href="#">Contact</a></li>
-            </ul>
-        </nav>
+        <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+            <nav>
+                <ul class="text-indigo-50 lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+                    <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                           href="#">Home</a>
+                    </li>
+                    <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                           href="#">Categories</a>
+                    </li>
+                    <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                           href="#">About</a>
+                    </li>
+                    <li>
+                        <a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
+                           href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="flex flex-col items-center py-5">
+        <h1 class="pt-2">LET'S DO IT TOGETHER</h1>
+        <p class="py-8">we will produce you to tunisian mandala</p>
+        <button
+            class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 pt-3 pb-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button">
+            See latest posts
+        </button>
     </div>
-</header>
+</div>
+<div id="categories" class="">
+
+    <ul class="flex flex-row flex-wrap md:mt-3 justify-center">
+        <li class="mx-8 mt-3"><a href="#">categorie 1</a></li>
+        <li class="mx-8 mt-3"><a href="#">categorie 2</a></li>
+        <li class="mx-8 mt-3"><a href="#">categorie 3</a></li>
+        <li class="mx-8 mt-3"><a href="#">categorie 4</a></li>
+        <li class="mx-8 mt-3"><a href="#">categorie 5</a></li>
+        <li class="mx-8 mt-3"><a href="#">categorie 6</a></li>
+    </ul>
+</div>
 </body>
 </html>
