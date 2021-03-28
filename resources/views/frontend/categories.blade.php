e20c781c4de4ce5c6086bd4153fff63f62626710
yosrigam <s33636@beuth-hochschule.de>
2021-03-29 00:32:59 +0200

    front end updates

--- resources/views/frontend/categories.blade.php
+++ resources/views/frontend/categories.blade.php
@@ -23,7 +23,7 @@
 </div>
 <div class="my-20">
     <div class="flex justify-center flex-row flex-wrap">
-        <div class="h-64 md:w-1/4 w-1/3 bg-gray-300 m-3 rounded-xl text-center flex flex-col justify-center bg-local bg-opacity-0" style="background-image: url(https://image.freepik.com/free-vector/background-template-with-mandala-pattern_1308-44077.jpg)"><h1 class="">Categorie</h1> </div>
+        <div class="h-64 md:w-1/4 w-1/3 bg-gray-300 m-3 rounded-xl text-center flex flex-col justify-center bg-local opacity-50" style="background-image: url(https://image.freepik.com/free-vector/background-template-with-mandala-pattern_1308-44077.jpg)"><h1 class="">Categorie</h1> </div>
         <div class="h-64 md:w-1/4 w-1/3 bg-gray-300 m-3 rounded-xl text-center flex flex-col justify-center bg-local opacity-50" style="background-image: url(https://image.freepik.com/free-vector/background-template-with-mandala-pattern_1308-44077.jpg)"><h1 class="">Categorie</h1> </div>
         <div class="h-64 md:w-1/4 w-1/3 bg-gray-300 m-3 rounded-xl text-center flex flex-col justify-center bg-local opacity-50" style="background-image: url(https://image.freepik.com/free-vector/background-template-with-mandala-pattern_1308-44077.jpg)"><h1 class="">Categorie</h1> </div>
         <div class="h-64 md:w-1/4 w-1/3 bg-gray-300 m-3 rounded-xl text-center flex flex-col justify-center bg-local opacity-50" style="background-image: url(https://image.freepik.com/free-vector/background-template-with-mandala-pattern_1308-44077.jpg)"><h1 class="">Categorie</h1> </div>
