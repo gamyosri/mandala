9af6e77b0703a54296fc3496aed886a6dc456eda
yosrigam <s33636@beuth-hochschule.de>
2021-03-29 22:34:18 +0200

    front end changes

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('frontend.home');
 });
-
-
-Route::get('/myspace', function () {
-    return view('backend.mySpace');
-});
-
 
 Route::get('/categories', function () {
     return view('frontend.categories');
 });
 
-
 Route::get('/about', function () {
     return view('frontend.about');
 });
 
-
 Route::get('/contact', function () {
     return view('frontend.contact');
 });
-
 
 Route::get('/myspace', function () {
     return view('backend.mySpace');
